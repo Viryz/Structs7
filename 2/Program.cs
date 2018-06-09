0f23f2fdb8c0cf8a1915bf6c0f6b8cb2450cda25
jul-ia <julie78sd@gmail.com>
2018-06-09 20:42:48 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass c = new MyClass();
+            c.change = "Not charged";
+            MyStruct s;
+            s.change = "Not changed";
+            Console.WriteLine(c.change);
+            Console.WriteLine(s.change);
+            ClassTaker(c);
+            StructTaker(ref s);
+            Console.WriteLine(c.change);
+            Console.WriteLine(s.change);
+
+            Console.ReadLine();
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "Changed";
+        }
+
+        static void StructTaker(ref MyStruct myStruct)
+        {
+            myStruct.change = "Changed";
+        }
+
+    }
+}
---
+++ Structs7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs7
+{
+    struct Train: IComparable
+    {
+        public string name;
+        public int numberOfTrain;
+        public string startTime;
+
+        public Train(string name, int numberOfTrain, string startTime)
+        {
+            this.name = name;
+            this.numberOfTrain = numberOfTrain;
+            this.startTime = startTime;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Train t = (Train)obj;
+            return numberOfTrain.CompareTo(t.numberOfTrain);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(numberOfTrain);
+            Console.WriteLine(startTime);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] trs = new Train[3];
+            for (int i = 0; i < trs.Length; i++)
+            {
+                string name, time;
+                int number;
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+                Console.Write("Enter number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write("Enter start time: ");
+                time = Console.ReadLine();
+                trs[i] = new Train(name, number, time);
+            }
+
+            Array.Sort(trs);
+
+            int num;
+            Console.Write("Enter number of train: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < trs.Length; i++)
+                if (trs[i].numberOfTrain == num)
+                    trs[i].Show();
+            
+            Console.ReadLine();
+        }
+    }
+}
